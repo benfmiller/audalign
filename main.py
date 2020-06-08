470f070554d4effd3bba619fb8c44a0b4b5ca0d3
Ben Miller <ben.f.miller24@gmail.com>
2020-06-07 21:38:11 -0600

    moved recognizer to recognize file

--- main.py
+++ main.py
@@ -2,47 +2,20 @@ def main():
     import audalign as ad
     import time
 
-    # TODO: Multiprocessing
+    # TODO: New fingerprint algorithm
     # TODO: Not double add files
-    # TODO: Add precision optional adjustments to recognize
     # TODO: Document
-    # TODO: Add plot function
     # TODO: Add uniquehashes and filename fields
 
-    ada = ad.Audalign("SUB.json")
+    ada = ad.Audalign("all_audio.json")
     # ada.write_processed_file("ResearchMaher/FraserSUB.mov", "processed_audio/FraserSUB.wav")
     t = time.time()
-    # ada.fingerprint_file("audio_files/TestAudio/SUB.wav") 
+    # ada.fingerprint_directory("audio_files") 
     t = time.time()-t
     print(f"It took {t} seconds to complete.")
     print(f"Total fingerprints: {ada.total_fingerprints}")
-    # ada.save_fingerprinted_files("SUB.json")
+    # ada.save_fingerprinted_files("all_audio.json")
     print(ada.recognize("audio_files/TestAudio/SUBtran1.wav"))
-    #ada.fingerprint_file("ResearchMaher/FraserStreet.mov",plot=True)
-    #ada.fingerprint_file("ResearchMaher/BenStreet.mp4",plot=True)
-    #ada.save_fingerprinted_files("all_audio.json")
-    # print(len(ada.fingerprinted_files[0][1]))
-    # djv.save_fingerprinted_files('Sub.json')
-    # print(len(djv.fingerprinted_files))
-
-    # print(djv.fingerprinted_files[0][0] + "  :  " + djv.fingerprinted_files[0][2])
-    # a = djv.fingerprinted_files
-    # b = a[0][1]
-    # print(b)
-
-    # print("\nBeginning Recognizing")
-    # print(djv.recognize(recognizer='MicrophoneRecognizer', seconds=10))
-    # print(djv.recognize("SUB.mp3"))
-
-    # djv.save_fingerprinted_files('test_mp3s.pickle')
-
-    # djv.fingerprint_directory("mp3")
-    # print(djv.fingerprinted_files)
-
-    # filen = djv.recognize("mp3/Sean-Fournier--Falling-For-You.mp3")
-    # print(filen)
-    # print ("From file we recognized: %s\n" % filen)
-
 
 if __name__ == "__main__":
     main()
