700addb904f0218e122c201e81b4a3934b1f908a
Ben Miller <ben.f.miller24@gmail.com>
2020-06-13 15:55:24 -0600

    won't double add files by filename

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     import audalign as ad
     import time
 
+    # TODO: autosaver?
     # TODO: New fingerprint algorithm
     # TODO: Not double add files
     # TODO: Document
@@ -10,12 +11,17 @@ def main():
     ada = ad.Audalign("all_audio.json")
     # ada.write_processed_file("ResearchMaher/FraserSUB.mov", "processed_audio/FraserSUB.wav")
     t = time.time()
+    # ada.fingerprint_file("audio_files/TestAudio/Rachel.mp4")
     # ada.fingerprint_directory("audio_files")
     t = time.time() - t
     print(f"It took {t} seconds to complete.")
     print(f"Total fingerprints: {ada.total_fingerprints}")
+    print(
+        f"Number of fingerprinted files: {len(ada.fingerprinted_files)} : {len(ada.file_names)}"
+    )
+
     # ada.save_fingerprinted_files("all_audio.json")
-    ada.plot("audio_files/TestAdio/Paige.MOV")
+    ada.plot("audio_files/TestAudio/Paige.MOV")
     print(ada.recognize("audio_files/TestAudio/Rachel.mp4"))
 
 
