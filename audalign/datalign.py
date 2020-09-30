222437123cf89e24622419b54638bd2bcae72eeb
Ben Miller <ben.f.miller24@gmail.com>
2020-09-29 19:07:03 -0600

    working on functions

--- audalign/datalign.py
+++ audalign/datalign.py
@@ -1,2 +1,7 @@
 def speed_of_sound(degrees : int) -> int:
     return 331.3 + (degrees * 0.606)
+
+def a_is_closer(offset_a : int, offset_b : int) -> bool:
+    return offset_a > offset_b
+
+def distance_from_event()
