a43c27ca93378209a237aecee4ae6f68c9228af9
Ben Miller <ben.f.miller24@gmail.com>
2020-11-17 13:41:47 -0700

    plot looks better

--- audalign/visrecognize.py
+++ audalign/visrecognize.py
@@ -33,21 +33,15 @@ def visrecognize(target_file_path: str, against_file_path: str):
     print(f"against height: {ah}")
     print(f"length of target: {len(target_arr2d)}")
 
-    m = mean_squared_error(target_arr2d[0:4000], against_arr2d[0:4000])
-    s = ssim(target_arr2d[0:4000], against_arr2d[0:4000])
+    m = mean_squared_error(
+        transposed_target_arr2d[0:4000], transposed_against_arr2d[0:4000]
+    )
+    s = ssim(transposed_target_arr2d[0:4000], transposed_against_arr2d[0:4000])
 
-    fig = plt.figure("Test")
-    # show first image
-    plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
-    ax = fig.add_subplot(1, 2, 1)
-    plt.imshow(target_arr2d)  # , cmap=plt.cm.gray) for gray colors
-    # plt.axis("off")
-    # show the second image
-    ax = fig.add_subplot(1, 2, 2)
-    plt.imshow(against_arr2d)  # , cmap=plt.cm.gray) for gray colors
-    # plt.axis("off")
-    # show the images
-    plt.show()
+    print(s)
+    print(m)
+
+    plot_two_images(target_arr2d, against_arr2d, mse=m, ssim_value=s)
 
     return results
 
@@ -71,52 +65,21 @@ def mse(imageA, imageB):
 """
 
 
-def plot_two_images(imageA, imageB, title="comparison", mse=None, ssim_value=None):
-    # compute the mean squared error and structural similarity
-    # index for the images
-    m, s = 0, 0
-    # m = mse(imageA, imageB)
-    # s = ssim(imageA, imageB)
+def plot_two_images(imageA, imageB, title="Comparison", mse=None, ssim_value=None):
     # setup the figure
+
     fig = plt.figure(title)
-    if mse and ssim_value:
-        plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+    if mse or ssim_value:
+        plt.suptitle(f"MSE: {mse:.4f}, SSIM: {ssim_value:.4f}")
     # show first image
     ax = fig.add_subplot(1, 2, 1)
     plt.imshow(imageA)  # , cmap=plt.cm.gray)
+    plt.gca().invert_yaxis()
     # plt.axis("off")
     # show the second image
     ax = fig.add_subplot(1, 2, 2)
     plt.imshow(imageB)  # , cmap=plt.cm.gray)
+    plt.gca().invert_yaxis()
     # plt.axis("off")
     # show the images
     plt.show()
-
-
-def included():
-    # load the images -- the original, the original + contrast,
-    # and the original + photoshop
-    original = cv2.imread("images/jp_gates_original.png")
-    contrast = cv2.imread("images/jp_gates_contrast.png")
-    shopped = cv2.imread("images/jp_gates_photoshopped.png")
-    # convert the images to grayscale
-    original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-    contrast = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
-    shopped = cv2.cvtColor(shopped, cv2.COLOR_BGR2GRAY)
-
-    # initialize the figure
-    fig = plt.figure("Images")
-    images = ("Original", original), ("Contrast", contrast), ("Photoshopped", shopped)
-    # loop over the images
-    for (i, (name, image)) in enumerate(images):
-        # show the image
-        ax = fig.add_subplot(1, 3, i + 1)
-        ax.set_title(name)
-        plt.imshow(image, cmap=plt.cm.gray)
-        plt.axis("off")
-    # show the figure
-    plt.show()
-    # compare the images
-    plot_two_images(original, original, "Original vs. Original")
-    plot_two_images(original, contrast, "Original vs. Contrast")
-    plot_two_images(original, shopped, "Original vs. Photoshopped")
