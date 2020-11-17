324f1799e3b666913cbdaa05620bccb8d756b4b4
Ben Miller <ben.f.miller24@gmail.com>
2020-11-16 18:25:45 -0700

    added visrecognize methods

--- audalign/visrecognize.py
+++ audalign/visrecognize.py
@@ -1,2 +1,78 @@
 import cv2
 import audalign.fingerprint
+
+
+def visrecognize(self, target_file_path: str, against_file_path: str):
+    results = {}
+    return results
+
+
+def visrecognize_directory(self, target_file_path: str, against_directory: str):
+    results = {}
+    return results
+
+
+# import the necessary packages
+from skimage.measure import structural_similarity as ssim
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def mse(imageA, imageB):
+    # the 'Mean Squared Error' between the two images is the
+    # sum of the squared difference between the two images;
+    # NOTE: the two images must have the same dimension
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
+
+    # return the MSE, the lower the error, the more "similar"
+    # the two images are
+    return err
+
+
+def compare_images(imageA, imageB, title):
+    # compute the mean squared error and structural similarity
+    # index for the images
+    m = mse(imageA, imageB)
+    s = ssim(imageA, imageB)
+    # setup the figure
+    fig = plt.figure(title)
+    plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+    # show first image
+    ax = fig.add_subplot(1, 2, 1)
+    plt.imshow(imageA, cmap=plt.cm.gray)
+    plt.axis("off")
+    # show the second image
+    ax = fig.add_subplot(1, 2, 2)
+    plt.imshow(imageB, cmap=plt.cm.gray)
+    plt.axis("off")
+    # show the images
+    plt.show()
+
+
+# load the images -- the original, the original + contrast,
+# and the original + photoshop
+original = cv2.imread("images/jp_gates_original.png")
+contrast = cv2.imread("images/jp_gates_contrast.png")
+shopped = cv2.imread("images/jp_gates_photoshopped.png")
+# convert the images to grayscale
+original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+contrast = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
+shopped = cv2.cvtColor(shopped, cv2.COLOR_BGR2GRAY)
+
+# initialize the figure
+fig = plt.figure("Images")
+images = ("Original", original), ("Contrast", contrast), ("Photoshopped", shopped)
+# loop over the images
+for (i, (name, image)) in enumerate(images):
+    # show the image
+    ax = fig.add_subplot(1, 3, i + 1)
+    ax.set_title(name)
+    plt.imshow(image, cmap=plt.cm.gray)
+    plt.axis("off")
+# show the figure
+plt.show()
+# compare the images
+compare_images(original, original, "Original vs. Original")
+compare_images(original, contrast, "Original vs. Contrast")
+compare_images(original, shopped, "Original vs. Photoshopped")
