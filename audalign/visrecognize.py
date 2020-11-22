e9b35f49e64f89b9a02ceb8867431c3e2df0ab01
Ben Miller <ben.f.miller24@gmail.com>
2020-11-21 17:17:37 -0700

    added frame width and overlap ratio and calculator for them

--- audalign/visrecognize.py
+++ audalign/visrecognize.py
@@ -4,23 +4,45 @@ from skimage.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import numpy as np
 
-from audalign.fingerprint import fingerprint
+import audalign.fingerprint as fingerprint
 from audalign.filehandler import read
 
 
-def visrecognize(target_file_path: str, against_file_path: str):
+def get_frame_width_and_overlap(
+    seconds_width: float, overlap_ratio: float
+) -> tuple(int, int):
+    seconds_width = max(
+        int(
+            seconds_width
+            // (
+                fingerprint.DEFAULT_WINDOW_SIZE
+                / fingerprint.DEFAULT_FS
+                * fingerprint.DEFAULT_OVERLAP_RATIO
+            )
+        ),
+        1,
+    )
+    overlap_ratio = max(int(overlap_ratio * seconds_width), 1)
+    return seconds_width, overlap_ratio
+
+
+def visrecognize(
+    target_file_path: str, against_file_path: str, img_width=1.0, overlap_ratio=0.5
+) -> dict:
     # With frequency of 44100
     # Each frame is 0.0929 seconds with an overlap ratio of .5,
     # so moving over one frame moves 0.046 seconds
     # 1 second of frames is 21.55 frames.
 
+    img_width, overlap_ratio = get_frame_width_and_overlap(img_width, overlap_ratio)
+
     results = {}
     target_samples, _ = read(target_file_path)
-    target_arr2d = fingerprint(target_samples, retspec=True)
+    target_arr2d = fingerprint.fingerprint(target_samples, retspec=True)
     transposed_target_arr2d = np.transpose(target_arr2d)
 
     against_samples, _ = read(against_file_path)
-    against_arr2d = fingerprint(against_samples, retspec=True)
+    against_arr2d = fingerprint.fingerprint(against_samples, retspec=True)
     transposed_against_arr2d = np.transpose(against_arr2d)
 
     # print(f"target max = {np.amax(target_arr2d)}")
@@ -32,6 +54,9 @@ def visrecognize(target_file_path: str, against_file_path: str):
     print(f"Target height: {th}, target width: {tw}")
     print(f"against height: {ah}")
     print(f"length of target: {len(target_arr2d)}")
+
+    # for i in range(0, tw-img_width, img_width):
+    #     for j in range(0, aw-img_width, img_width):
 
     m = mean_squared_error(
         transposed_target_arr2d[0:4000], transposed_against_arr2d[0:4000]
