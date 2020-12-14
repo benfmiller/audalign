533ae5b90fa600fb3677705c4341a6eaf221511e
Ben Miller <ben.f.miller24@gmail.com>
2020-12-14 14:21:23 -0700

    added tests to test audalign

--- tests/test_audalign.py
+++ tests/test_audalign.py
@@ -10,6 +10,7 @@ class TestInit:
 
     test_file = "audio_files/TestAudio/test.wav"
 
+    @pytest.mark.smoke
     def test_initialization(self):
 
         ada = ad.Audalign()
@@ -17,6 +18,61 @@ class TestInit:
         ada2 = ad.Audalign("all_audio.json")
         assert ada2.total_fingerprints > 0
         assert len(ada2.fingerprinted_files) > 0
+
+    def test_filter_duplicates(self):
+        ada1 = ad.Audalign()
+
+        ada1.load_fingerprinted_files("all_audio_panako.json")
+        a = len(ada1.file_names)
+        b = ada1.total_fingerprints
+        c = len(ada1.fingerprinted_files)
+        ada1.load_fingerprinted_files("all_audio_panako.json")
+        ada1.filter_duplicates()
+        assert a == len(ada1.file_names)
+        assert b == ada1.total_fingerprints
+        assert c == len(ada1.fingerprinted_files)
+
+    def test_clear(self):
+        ada = ad.Audalign("all_audio.json")
+        assert len(ada.fingerprinted_files) > 0
+        ada.clear_fingerprints()
+        assert len(ada.fingerprinted_files) == 0
+        assert len(ada.file_names) == 0
+        assert ada.total_fingerprints == 0
+
+    def test_set_accuracy(self):
+        ada = ad.Audalign()
+        assert ada.get_accuracy() == 2
+        assert ad.fingerprint.default_amp_min == 65
+        ada.set_accuracy(3)
+        ada.set_accuracy(4)
+        ada.set_accuracy(1)
+        assert ada.get_accuracy() == 1
+        assert ad.fingerprint.default_amp_min == 80
+
+    def test_set_num_processors(self):
+        ada = ad.Audalign(num_processors=1)
+        assert ada.num_processors == 1
+        ada.set_num_processors(80)
+        assert ada.num_processors == 80
+
+    def test_write_and_load(self):
+        ada = ad.Audalign("all_audio_panako.json")
+        assert len(ada.file_names) > 0
+        ada.save_fingerprinted_files("test_save_fingerprints.json")
+        ada.save_fingerprinted_files("test_save_fingerprints.pickle")
+        ada.clear_fingerprints()
+        assert len(ada.file_names) == 0
+        ada.load_fingerprinted_files("test_save_fingerprints.pickle")
+        assert len(ada.file_names) > 0
+        ada.clear_fingerprints()
+        assert len(ada.file_names) == 0
+        ada.load_fingerprinted_files("test_save_fingerprints.json")
+        assert len(ada.file_names) > 0
+
+
+class TestFingerprinting:
+    test_file = "audio_files/TestAudio/test.wav"
 
     def test_fingerprint_file(self):
         ada = ad.Audalign()
@@ -34,6 +90,7 @@ class TestInit:
         ada.fingerprint_file(self.test_file, set_file_name="Sup")
         assert ada.file_names[0] == "Sup"
 
+    @pytest.mark.smoke
     def test_fingerprint_directory(self):
         ada_multi = ad.Audalign()
         ada_multi.fingerprint_directory("audio_files/processed_audio")
@@ -44,16 +101,3 @@ class TestInit:
         ada_single.fingerprint_directory("audio_files/processed_audio")
         assert ada_single.total_fingerprints > 0
         assert len(ada_single.fingerprinted_files) > 0
-
-    def test_filter_duplicates(self):
-        ada1 = ad.Audalign()
-
-        ada1.load_fingerprinted_files("all_audio_panako.json")
-        a = len(ada1.file_names)
-        b = ada1.total_fingerprints
-        c = len(ada1.fingerprinted_files)
-        ada1.load_fingerprinted_files("all_audio_panako.json")
-        ada1.filter_duplicates()
-        assert a == len(ada1.file_names)
-        assert b == ada1.total_fingerprints
-        assert c == len(ada1.fingerprinted_files)
