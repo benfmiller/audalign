fbffccb9833c01d9805d63672d37faf6daf38568
Ben Miller <ben.f.miller24@gmail.com>
2020-12-15 13:45:16 -0700

    moved align to recognize file

--- tests/test_recognize.py
+++ tests/test_recognize.py
@@ -4,39 +4,52 @@ import pytest
 test_file = "audio_files/TestAudio/test.wav"
 
 
-@pytest.mark.smoke
-def test_recognize():
+class TestRecognize:
+    @pytest.mark.smoke
+    def test_recognize(self):
 
-    ada = ad.Audalign("all_audio_panako.json")
-    assert ada.total_fingerprints > 0
+        ada = ad.Audalign("all_audio_panako.json")
+        assert ada.total_fingerprints > 0
 
-    ada.fingerprinted_files[0][0] = "different"
-    ada.file_names[0] = "different"
+        ada.fingerprinted_files[0][0] = "different"
+        ada.file_names[0] = "different"
 
-    result = ada.recognize(test_file)
-    assert len(result) > 1
+        result = ada.recognize(test_file)
+        assert len(result) > 1
 
-    result2 = ada.recognize("audio_files/TestAudio/pink_noise.wav", filter_matches=3)
-    assert not result2
+        result2 = ada.recognize(
+            "audio_files/TestAudio/pink_noise.wav", filter_matches=3
+        )
+        assert not result2
+
+    @pytest.mark.smoke
+    def test_visrecognize(self):
+        ada = ad.Audalign()
+        results = ada.visrecognize(
+            test_file,
+            test_file,
+            img_width=0.5,
+            volume_threshold=215,
+        )
+        assert results
+
+    def test_visrecognize_directory(self):
+        ada = ad.Audalign()
+        results = ada.visrecognize_directory(
+            test_file,
+            "audio_files/processed_audio",
+            img_width=0.5,
+            volume_threshold=215,
+        )
+        assert results
 
 
-def test_visrecognize():
-    ada = ad.Audalign()
-    results = ada.visrecognize(
-        test_file,
-        test_file,
-        img_width=0.5,
-        volume_threshold=215,
-    )
-    assert results
+class TestAlign:
+    def test_align(self):
+        ada = ad.Audalign()
+        result = ada.align("audio_files/shifts", "test_alignment")
+        assert result
 
-
-def test_visrecognize_directory():
-    ada = ad.Audalign()
-    results = ada.visrecognize_directory(
-        test_file,
-        "audio_files/processed_audio",
-        img_width=0.5,
-        volume_threshold=215,
-    )
-    assert results
+    # def test_target_align_fingerprint(self):
+    #     ada = ad.Audalign()
+    #     result = ada.target_align("")
