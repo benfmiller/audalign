7a01eae70adebf0c7d069f91991502435cbe32f5
Ben Miller <benfmiller132@gmail.com>
2021-03-28 14:25:21 -0600

    added spectrogram to plot

--- audalign/correcognize.py
+++ audalign/correcognize.py
@@ -10,19 +10,27 @@ def correcognize(
     start_end_target: tuple = None,
     start_end_against: tuple = None,
     filter_matches: int = 0,
+    sample_rate: int = fingerprint.DEFAULT_FS,
     plot: bool = False,
 ):
 
     target_array, _ = read(target_file_path)
     against_array, _ = read(against_file_path)
 
-    correlation = against_array
+    # target_array = signal.butter()
+    # correlation = signal.convolve(target_array, against_array)
+    # correlation = signal.correlate(target_array, against_array)
+    correlation = target_array
+    # signal.coh
+    # plt.xcorr(target_array, against_array)
+    # plt.show()
 
     if plot:
         plot_cor(
             array_a=target_array,
             array_b=against_array,
             corr_array=correlation,
+            sample_rate=sample_rate,
             arr_a_title=target_file_path,
             arr_b_title=against_file_path,
         )
@@ -34,6 +42,7 @@ def correcognize_directory(
     against_directory: str,
     start_end: tuple = None,
     filter_matches: int = 0,
+    sample_rate: int = fingerprint.DEFAULT_FS,
     plot: bool = False,
 ):
     print(f"{target_file_path} : {against_directory}")
@@ -44,13 +53,14 @@ def plot_cor(
     array_a,
     array_b,
     corr_array,
+    sample_rate,
     title="Comparison",
     arr_a_title=None,
     arr_b_title=None,
 ):
     fig = plt.figure(title)
 
-    fig.add_subplot(3, 1, 1)
+    fig.add_subplot(3, 2, 1)
     plt.plot(array_a)
     plt.xlabel("Sample Index")
     plt.ylabel("Amplitude")
@@ -58,7 +68,14 @@ def plot_cor(
     if arr_a_title:
         plt.title(arr_a_title)
 
-    fig.add_subplot(3, 1, 2)
+    arr2d_a = fingerprint.fingerprint(array_a, fs=sample_rate, retspec=True)
+    fig.add_subplot(3, 2, 2)
+    plt.imshow(arr2d_a)  # , cmap=plt.cm.gray)
+    plt.gca().invert_yaxis()
+    if arr_a_title:
+        plt.title(arr_a_title)
+
+    fig.add_subplot(3, 2, 3)
     plt.plot(array_b)
     plt.xlabel("Sample Index")
     plt.ylabel("Amplitude")
@@ -66,7 +83,14 @@ def plot_cor(
     if arr_b_title:
         plt.title(arr_b_title)
 
-    fig.add_subplot(3, 1, 3)
+    arr2d_b = fingerprint.fingerprint(array_b, fs=sample_rate, retspec=True)
+    fig.add_subplot(3, 2, 4)
+    plt.imshow(arr2d_b)
+    plt.gca().invert_yaxis()
+    if arr_b_title:
+        plt.title(arr_b_title)
+
+    fig.add_subplot(3, 2, 5)
     plt.plot(corr_array)
     plt.title(f"Correlation")
     plt.xlabel("Sample Index")
