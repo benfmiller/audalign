410eae4ffd04c6a85eb0e59f0c7ee8ab4bedfe3c
Ben Miller <benfmiller132@gmail.com>
2021-06-24 15:10:23 -0600

    calc technique and rank alignment

--- audalign/datalign.py
+++ audalign/datalign.py
@@ -1,11 +1,39 @@
-import audalign
-
 """Alignment isn't quite accurate enough for this yet"""
 
-# def _create_audainstance()
+
+def rank_alignment(alignment):
+    ranks = {}
+    if "fine_match_info" in alignment.keys():
+        ranks["fine_match_info"] = _rank_alignment(alignment["fine_match_info"])
+    ranks["match_info"] = _rank_alignment(alignment=alignment["match_info"])
+    return ranks
+
+
+def _rank_alignment(alignment):
+    # TODO
+    ...
+
+
+def rank_recognition(
+    alignment,
+):
+    # TODO
+    ...
+
+
+def calc_technique(keys):
+    if "confidence" in keys and "scaling_factor" not in keys:
+        return "fingerprints"
+    elif "confidence" not in keys:
+        return "visual"
+    elif "offset_frames" in keys:
+        return "correlation_spectrogram"
+    else:
+        return "correlation"
 
 
 def speed_of_sound(degrees: int) -> int:
+    """degrees in celcius"""
     return 331.3 + (degrees * 0.606)
 
 
@@ -23,6 +51,3 @@ def angle_two_events():
 
 def which_is_first():
     ...
-
-
-# def distance_to_event_given
