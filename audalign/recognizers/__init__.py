94af0daad734fd403ae68776ddd525c71aba5988
Ben Miller <benfmiller132@gmail.com>
2021-10-24 22:05:34 -0600

    adding base classes

--- audalign/recognizers/__init__.py
+++ audalign/recognizers/__init__.py
@@ -5,5 +5,11 @@ This is the base recognizer class that is handed to the recognition methods
 from abc import ABC
 
 
+class BaseConfig(ABC):
+    pass
+
+
 class BaseRecognizer(ABC):
     pass
+
+    def recognize()
